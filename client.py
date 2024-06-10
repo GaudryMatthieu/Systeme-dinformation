0a5d010b19b471d73229cc36fd7632c8c0382d89
matthieu <matthieu.gaudry78@gmail.com>
2024-06-10 10:48:26 +0200

    update of the client server

--- client.py
+++ client.py
@@ -1,11 +1,30 @@
 import socket
-import sys
+#import sys
 
 HOST, PORT = "localhost", 9999
 
-# Joindre les arguments de la ligne de commande pour former l'action
-action = " ".join(sys.argv[1:])
-data = [action]
+data = []
+
+def send_data():
+    if data:
+        data_str = "$".join(data)
+
+        # Créer une socket (SOCK_STREAM signifie une socket TCP)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+            # Se connecter au serveur et envoyer les données
+                sock.connect((HOST, PORT))
+                sock.sendall(bytes(data_str, "utf-8"))
+
+                # Recevoir les données du serveur et fermer la connexion
+                received = str(sock.recv(1024), "utf-8")
+
+                print("Sent:     {}".format(data_str))
+                print("Received: {}".format(received))
+            except ConnectionError as e:
+                print(f"Failed to connect to server: {e}")
+    else:
+        print("No data to send.")
 
 def input_book():
     author = input("Entrez l'auteur : ")
@@ -15,26 +34,28 @@ def input_book():
     data.append(title)
     data.append(content)
 
-if action == "créer" or action == "modifier" or action == "suppr":
-    input_book()
-
-# Vérifier si data est non vide avant de joindre et d'envoyer
-if data:
-    data_str = "$".join(data)
-
-    # Créer une socket (SOCK_STREAM signifie une socket TCP)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        try:
-            # Se connecter au serveur et envoyer les données
-            sock.connect((HOST, PORT))
-            sock.sendall(bytes(data_str, "utf-8"))
-
-            # Recevoir les données du serveur et fermer la connexion
-            received = str(sock.recv(1024), "utf-8")
-
-            print("Sent:     {}".format(data_str))
-            print("Received: {}".format(received))
-        except ConnectionError as e:
-            print(f"Failed to connect to server: {e}")
-else:
-    print("No data to send.")
+action = ""
+while action != "q":
+        action = input("choose action: ")
+        print(f"triggered action: {action}")
+        match action:
+            case "modifier":
+                data = [action]
+                input_book()
+                send_data()
+                data = []
+            case "créer":
+                data = [action]
+                input_book()
+                send_data()
+                data = []
+            case "suppr":
+                data = [action]
+                send_data()
+                data = []
+            case "lire":
+                data = [action]
+                send_data()
+                data = []
+            case _:
+                pass
