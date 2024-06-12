6956ec485a0afb2217ef3fa041a4a1a08a1d5419
matthieu <matthieu.gaudry78@gmail.com>
2024-06-12 14:09:09 +0200

    fix command for docker

--- postgre/command.sh
+++ postgre/command.sh
@@ -1,3 +1,3 @@
 # How to start a docker container with a yaml file
 # Use docker compose and the path to the yaml file and up
-docker compose -f .\postgre\postgresql-adminer.yml upv
+docker compose -f .\postgre\postgresql-adminer.yml up
