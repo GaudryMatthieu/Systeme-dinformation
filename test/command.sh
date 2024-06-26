bcfa0206a225a9d7e473ff8442b1153fa58ed28b
matthieu <matthieu.gaudry78@gmail.com>
2024-06-26 09:51:18 +0200

    forget commit

--- test/command.sh
+++ test/command.sh
@@ -13,3 +13,9 @@ docker pull prom/alertmanager:latest
 docker run --name alertmanager -d -p 127.0.0.1:9093:9093 quay.io/prometheus/alertmanager
 
 docker run -d -v "$pwd/test/prometheus.yml:/etc/prometheus/prometheus.yml" -v C:\Users\matth\HEXAGONE\2e-annee\systeme-dinformation\cours\test\prometheus.yml -p 9090:9090 prom/prometheus
+
+# command to build Grafana (OPtional)
+docker pull grafana/grafana:latest
+
+# Run Grafana (login: admin/admin)
+docker run -d -p 3000:3000 --name=grafana grafana/grafana-enterprise
